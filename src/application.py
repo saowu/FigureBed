f90bbfcbcdeee16fee31bbab1e9fb1bf3f5ad346
saowu <786021717@qq.com>
2020-03-01 18:36:46 +0800

    allowed_file

--- src/application.py
+++ src/application.py
@@ -14,8 +14,14 @@ from src.file_mode import FileMode, file2dict
 app = Flask('FigureBed')
 
 app.config['UPLOAD_FOLDER'] = os.path.dirname(os.path.dirname(__file__)) + '/uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 CORS(app, supports_credentials=True)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 @app.route('/', methods=['GET'])
@@ -28,11 +34,12 @@ def update():
     getlist = request.files.getlist('files')
     files = []
     for file in getlist:
-        now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
-        filename = file.filename
-        name = str(now) + filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
-        files.append(FileMode(filename, 'http://127.0.0.1:8000/upload/' + name))
+        if file and allowed_file(file.filename):
+            now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+            filename = file.filename
+            name = str(now) + filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
+            files.append(FileMode(filename, 'http://127.0.0.1:8000/upload/' + name))
     return json.dumps(files, default=file2dict, )
 
 
