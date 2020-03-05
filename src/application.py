6b256aabae3db55dee27abdf700db3355d46597c
saowu <786021717@qq.com>
2020-03-05 20:55:39 +0800

    load image

--- src/application.py
+++ src/application.py
@@ -6,10 +6,12 @@
 __author__ = 'saowu'
 
 import json, time, os
-from flask import Flask, url_for, render_template, request, jsonify
+import re
+
+from flask import Flask, url_for, render_template, request, jsonify, Response, abort
 from flask_cors import CORS
 
-from src.file_mode import FileMode, file2dict
+from src.mode import FileMode, file2dict
 
 app = Flask('FigureBed')
 
@@ -22,6 +24,14 @@ CORS(app, supports_credentials=True)
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+
+def get_file(filename):
+    listdir = os.listdir(app.config['UPLOAD_FOLDER'])
+    for name in listdir:
+        if re.match(filename + '.*', name):
+            filename = name
+    return [open(app.config['UPLOAD_FOLDER'] + filename, 'rb'), "image/" + name.rsplit('.', 1)[1]]
 
 
 @app.route('/', methods=['GET'])
@@ -39,8 +49,14 @@ def update():
             filename = file.filename
             name = str(now) + filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
-            files.append(FileMode(filename, 'http://127.0.0.1:8000/upload/' + name))
+            files.append(FileMode(filename, 'http://127.0.0.1:8000/image/' + name.rsplit('.', 1)[0]))
     return json.dumps(files, default=file2dict, )
+
+
+@app.route('/image/<filename>', methods=['GET'])
+def get_images(filename):
+    image = get_file(filename)
+    return Response(image[0], mimetype=str(image[1]))
 
 
 if __name__ == '__main__':
