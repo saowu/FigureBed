bd3cd96baac9cd12b662f4998c604ca8af0e4715
saowu <786021717@qq.com>
2020-03-24 23:14:51 +0800

    update logging

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,14 @@ app.config.from_object(LogConfig())
 # 注册
 app.register_blueprint(home_blueprint)
 
+# 创建image,csv,log目录
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(r'' + app.config['UPLOAD_FOLDER'])
+if not os.path.exists(app.config['RECORD_FOLDER']):
+    os.makedirs(r'' + app.config['RECORD_FOLDER'])
+if not os.path.exists(app.config['LOG_FOLDER']):
+    os.makedirs(r'' + app.config['LOG_FOLDER'])
+
 # 初始化log
 log_file_str = app.config['LOG_FOLDER'] + app.config['LOG_FILENAME']
 log_formatter = logging.Formatter(
@@ -40,14 +48,6 @@ app.logger.addHandler(handler)
 # 初始化数据库连接池
 db = dbutils.DBUtil(app.config['DB_HOST'], app.config['DB_PORT'], app.config['DATABASE'], app.config['USER_NAME'],
                     app.config['PASSWORD'])
-
-# 创建image,csv,log目录
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(r'' + app.config['UPLOAD_FOLDER'])
-if not os.path.exists(app.config['RECORD_FOLDER']):
-    os.makedirs(r'' + app.config['RECORD_FOLDER'])
-if not os.path.exists(app.config['LOG_FOLDER']):
-    os.makedirs(r'' + app.config['LOG_FOLDER'])
 
 # 定时任务
 init_scheduler(app)
